fc0eff9dccaf88ced664d1298fadacedff05ea92
An nguyễn <theesan2k1@gmail.com>
2022-05-24 13:17:18 +0700

    nexxt

--- OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
+++ OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using OnlineShop.Data;
+using OnlineShop.Models;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -19,5 +20,25 @@ namespace OnlineShop.Areas.Admin.Controllers
         {
             return View(_db.ProductTypes.ToList());
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product type has been saved";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+
+
     }
 }
